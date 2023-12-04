7890c873ff996b87649b44ec5a287a1cd194d8a3
Felipe Matos <felipematosapps@gmail.com>
2023-12-04 13:19:06 -0300

    Inserindo registros na tabela

--- app/src/main/java/com/example/sqliteandroid/MainActivity.kt
+++ app/src/main/java/com/example/sqliteandroid/MainActivity.kt
@@ -4,26 +4,48 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.sqliteandroid.database.DatabaseHelper
+import com.example.sqliteandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
 
     private val bancoDados by lazy {
         DatabaseHelper(this)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        with(binding){
+            btnSalvar.setOnClickListener{
+                salvar()
+            }
+
+            btnListar.setOnClickListener {
+                listar()
+            }
+        }
+
+    }
+
+    private fun listar() {
+
+    }
+
+   private fun salvar(){
+
+       val titulo = binding.editProduto.text.toString()
+       val sql = "INSERT into produtos VALUES (null,'$titulo', 'Descricao..');"
 
         try {
-            bancoDados.writableDatabase.execSQL(
-                "INSERT into produtos VALUES (null,'Notebook Acer', 'Descricao..');"
-            )
+            bancoDados.writableDatabase.execSQL(sql)
             Log.i("info_db", "Sucesso ao inserir ")
         }catch (e:Exception){
             Log.i("info_db", "Error ao inserir : ")
 
         }
-
-
     }
 }
