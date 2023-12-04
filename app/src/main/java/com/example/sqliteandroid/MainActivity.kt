8827056c5f60acf0f938210d9dcf13a267ab7fa8
Felipe Matos <felipematosapps@gmail.com>
2023-12-04 12:02:06 -0300

    Criando tabela

--- app/src/main/java/com/example/sqliteandroid/MainActivity.kt
+++ app/src/main/java/com/example/sqliteandroid/MainActivity.kt
@@ -2,14 +2,28 @@ package com.example.sqliteandroid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.sqliteandroid.database.DatabaseHelper
 
 class MainActivity : AppCompatActivity() {
+
+    private val bancoDados by lazy {
+        DatabaseHelper(this)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        try {
+            bancoDados.writableDatabase.execSQL(
+                "INSERT into produtos VALUES (null,'Notebook Acer', 'Descricao..');"
+            )
+            Log.i("info_db", "Sucesso ao inserir ")
+        }catch (e:Exception){
+            Log.i("info_db", "Error ao inserir : ")
 
-        val dbHelper = DatabaseHelper(this)
+        }
+
+
     }
 }
--- app/src/main/java/com/example/sqliteandroid/database/DatabaseHelper.kt
+++ app/src/main/java/com/example/sqliteandroid/database/DatabaseHelper.kt
@@ -3,23 +3,36 @@ package com.example.sqliteandroid.database
 import android.content.Context
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
 
 
 class DatabaseHelper(context : Context) : SQLiteOpenHelper (
     //1 Contexto , 2)Nome do Banco de Dados
     //3 CursorFactory 4)Versão do banco de dados
-    context,"loja.db", null ,1
+    context,"loja.db", null ,2
 ) {
 
     //Metódo chamado uma única vez
     override fun onCreate(db: SQLiteDatabase?) {
-
-
+        Log.i("info_db", "Executou o onCreate : ")
+        val sql = "CREATE TABLE IF NOT EXISTS produtos (" +
+                "id_produto INTEGER not NULL PRIMARY KEY AUTOINCREMENT," +
+                "titulo VARCHAR (100)," +
+                "descricao text " +
+                ");"
+       try {
+           db?.execSQL(sql)
+           Log.i("info_db", "Sucesso ao criar a tabela : ")
+       }catch (e : Exception){
+            e.printStackTrace()
+           Log.i("info_db", "Error ao criar a tabela : ")
+       }
 
     }
 
     //Metódo chamado quando tiver um nova versão
     override fun onUpgrade(db: SQLiteDatabase?, p1: Int, p2: Int) {
+        Log.i("info_db", "Executou o onUpgrade : ")
         //cria a tabela de vendas
         TODO("Not yet implemented")
     }
